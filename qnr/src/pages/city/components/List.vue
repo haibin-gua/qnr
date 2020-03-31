a5f0d776d07e541d710218b196862a11cea90ccf
haibin-gua <1305263409@qq.com>
2020-03-31 14:40:36 +0800

    qnr

--- qnr/src/pages/city/components/List.vue
+++ qnr/src/pages/city/components/List.vue
@@ -1,5 +1,6 @@
 <template>
-  <div class="list">
+  <div class="list" ref="wrapper">
+   <div>
     <div class="area">
       <div class="title border-topbottm">当前城市</div>
       <div class="button-list">
@@ -11,41 +12,41 @@
     <div class="area">
       <div class="title border-topbottm">热门城市</div>
       <div class="button-list">
-        <div class="button-wrapper">
-          <div class="button">北京</div>
-          </div>
-        <div class="button-wrapper">
-          <div class="button">北京</div>
-          </div>
-        <div class="button-wrapper">
-          <div class="button">北京</div>
-          </div>
-        <div class="button-wrapper">
-          <div class="button">北京</div>
-          </div>
-         <div class="button-wrapper">
-           <div class="button">北京</div>
-           </div>
-         <div class="button-wrapper">
-           <div class="button">北京</div>
-           </div>
-         <div class="button-wrapper">
-           <div class="button">北京</div>
-           </div>
+        <div class="button-wrapper" v-for="item of hot" :key="item.id">
+          <div class="button">{{item.name}}</div>
+        </div>
       </div>
     </div>
-    <div class="area">
-      <div class="title border-bottom">A</div>
+    <div class="area" v-for="(item,key) of cities" :key="key" :ref="key">
+      <div class="title border-bottom">{{key}}</div>
       <div class="item-list">
-        <div class="item border-topbottom">阿拉尔</div>
+        <div class="item border-topbottom" v-for="innerItem of item" :key="innerItem.id">{{innerItem.name}}</div>
       </div>
     </div>
+   </div>
   </div>
 </template>
 
 <script>
+import Bscroll from 'better-scroll'
 export default {
-  name:'CityList'
+  name:'CityList',
+  props:{
+    hot:Array,
+    cities:Object,
+    letter:String
+  },
+  mounted () {
+    this.scroll = new Bscroll(this.$refs.wrapper)
+  },
+  watch:{
+    letter () {
+      if(this.letter){
+        const element = this.$refs[this.letter][0]
+        this.scroll.scrollToElement(element)
+      }
+    }
+  }
 }
 </script>
 
@@ -58,27 +59,34 @@ export default {
   .border-bottom
     &:before
       border-color #CCCCCC
-  .title
-    line-height :.44rem
-    background:#eee
-    padding-left:.2rem
-    color:#666
-    font-size :.26rem
-  .button-list
-    overflow hidden
-    padding :.1rem .6rem .1rem .1rem
-  .button-wrapper
-    float:left
-    width:33.33%
-    .button
-      margin:.1rem
-      padding:.1rem 0
-      text-align :center
-      border:.02rem solid #ccc
-      border-radius :.06rem
-  .item-list
-    .item
-      line-height :.76rem
-      color:#666
+  .list
+    overflow :hidden
+    position:absolute
+    top:1.58rem
+    left:0
+    right:0
+    bottom:0
+    .title
+      line-height :.54rem
+      background:#eee
       padding-left:.2rem
+      color:#666
+      font-size :.26rem
+    .button-list
+      overflow hidden
+      padding :.1rem .6rem .1rem .1rem
+    .button-wrapper
+      float:left
+      width:33.33%
+      .button
+        margin:.1rem
+        padding:.1rem 0
+        text-align :center
+        border:.02rem solid #ccc
+        border-radius :.06rem
+    .item-list
+      .item
+        line-height :.76rem
+        color:#666
+        padding-left:.2rem
 </style>
